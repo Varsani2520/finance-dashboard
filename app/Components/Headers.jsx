b8e68968f6ebbbadcefb78c703009db876be697a
Varsani2520 <varsaniranjani2520@gmail.com>
2024-06-28 11:23:03 +0530

    header in admin route done

---
+++ app/Components/Headers.jsx
@@ -0,0 +1,27 @@
+'use client'
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+
+
+
+export default function BasicBreadcrumbs() {
+  return (
+    <div role="presentation"  style={{margin:'40px'}}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="black" href="/#">
+          Pages
+        </Link>
+        <Link
+          underline="hover"
+          color="black"
+          href="/#"
+        >
+          Main Dashboard
+        </Link>
+      </Breadcrumbs>
+        <Typography color="text.primary" style={{fontSize:"34px",fontWeight:'bold'}}>Main Dashboard</Typography>
+    </div>
+  );
+}
---
+++ app/admin/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Headers from '../Components/Headers'
+
+const page = () => {
+  return (
+    <div>
+      <Headers/>
+    </div>
+  )
+}
+
+export default page
