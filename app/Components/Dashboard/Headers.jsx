7786e92bef316c1f9fc2642fcd17d4ffa115e3ca
Soni Dhruv <114583978+dhruv8433@users.noreply.github.com>
2024-06-29 21:37:24 +0530

    Update Headers.jsx

--- app/Components/Dashboard/Headers.jsx
+++ app/Components/Dashboard/Headers.jsx
@@ -47,7 +47,7 @@ export default function BasicBreadcrumbs({ title, route }) {
   ];
 
   const user = useSelector((state) => state.auth.message);
-  let userInfo = user ? useSelector((state) => state.auth.authUser.data.username) : "";
+  let userInfo = user === true ? useSelector((state) => state.auth.authUser.data.username) : "";
 
   console.log(userInfo);
 
