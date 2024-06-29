5886733687c22b02829ef2ab33a12d7305d20b8e
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-06-29 17:50:17 +0530

    upadte code and minor chnages

--- app/finance/page.jsx
+++ app/finance/page.jsx
@@ -53,7 +53,7 @@ const Finance = () => {
           </Typography>
         ) : (
           <Grid container spacing={3}>
-            {stockData.map((stock, index) => (
+            {stockData && stockData.map((stock, index) => (
               <Grid item xs={12} sm={6} md={4} key={index}>
                 <Card
                   sx={{ bgcolor: theme.palette.background.card }}
